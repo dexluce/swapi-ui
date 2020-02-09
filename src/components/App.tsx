b8ae59f3521e43e0f2620b2886623fdb1c8fe392
Balthazar de Moncuit <balthazar.de.moncuit@tree-it.ch>
2020-02-09 01:34:20 +0100

    Add StarWars page & add routing fonctionnality

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,13 +1,22 @@
 import React from 'react';
+import Login from './Login';
 import './App.css';
+import PrivateRoute from '../routes/PrivateRoute';
+import StarWarsList from './StarWarsList';
+import { Route, BrowserRouter, Switch } from 'react-router-dom';
 
 export default class App extends React.Component {
   render() {
-      return (<div className="App">
-        <header className="App-header">
+    return <div className="App">
+      <header className="App-header">
 
-        </header>
-      </div>
-    );
+      </header>
+      <BrowserRouter>
+        <Switch>
+          <PrivateRoute exact path="/" component={StarWarsList}/>
+          <Route path="/login" component={Login}/>
+        </Switch>
+      </BrowserRouter>
+    </div>
   }
 }
