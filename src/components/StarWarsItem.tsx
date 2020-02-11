b69bec68309087a30f9cdff80dfb3fd4b01f6192
Balthazar de Moncuit <balthazar.de.moncuit@tree-it.ch>
2020-02-11 13:01:32 +0100

    Add a starwarsItem modal

--- src/components/StarWarsItem.tsx
+++ src/components/StarWarsItem.tsx
@@ -1,30 +1,93 @@
 import React, { Component } from 'react';
-import { Film, Species, People, Planet, Starship, Vehicle } from '../store/types';
-import { ListItem, ListItemText } from '@material-ui/core';
+import { Item } from '../store/types';
+import { ListItem, ListItemText, Modal } from '@material-ui/core';
+import CSS from 'csstype';
 
-export default class StarWarsItem extends Component<{item: Film | Species | People | Planet | Starship | Vehicle}> {
+export default class StarWarsItem extends Component<{item: Item}> {
+  state = { open: false }
+  modalStyle: CSS.Properties = {
+    display: 'flex',
+    padding: "10px",
+    alignItems: 'center',
+    justifyContent: 'center',
+  };
+
+  modalContentStyle: CSS.Properties = {
+    width: "400px",
+    backgroundColor: "white",
+    border: "solid 1px",
+    overflowX: "hidden",
+    overflowY: "auto",
+  }
+
+  itemStyle: CSS.Properties = {
+    textAlign: "center",
+    cursor: "pointer",
+  }
+
+  displayItemDetails() {
+    for (let [key, value] of Object.entries(this.props.item)) {
+      console.log(`${key}: ${value}`);
+    }
+    this.setState({ open: true });
+  }
+
+  closeItemDetails() {
+    console.log("closeItemDetails");
+    this.setState({ open: false });
+  }
+
+  toCapitalizedWords(name: string) {
+    var words = name.match(/[A-Za-z][a-z]*/g) || [];
+
+    return words.map(this.capitalize).join(" ");
+  }
+
+  capitalize(word: string) {
+    return word.charAt(0).toUpperCase() + word.substring(1);
+  }
+
   render() {
     return (
-      <ListItem style={{textAlign: "center"}}>
-        {(() => {
-          switch (this.props.item.type) {
-            case "People":
-              return (<ListItemText>{this.props.item.name}</ListItemText>);
-            case "Film":
-              return (<ListItemText>{this.props.item.title}</ListItemText>);
-            case "Planet":
-              return (<ListItemText>{this.props.item.name}</ListItemText>);
-            case "Species":
-              return (<ListItemText>{this.props.item.name}</ListItemText>);
-            case "Starship":
-              return (<ListItemText>{this.props.item.name}</ListItemText>);
-            case "Vehicle":
-              return (<ListItemText>{this.props.item.name}</ListItemText>);
-            default:
-              return null;
-          }
-        })()}
-      </ListItem>
+      <span>
+        <ListItem className="listItem" style={this.itemStyle} onClick={() => this.displayItemDetails()}>
+          {(() => {
+            switch (this.props.item.type) {
+              case "People":
+                return (<ListItemText>{this.props.item.name}</ListItemText>);
+              case "Film":
+                return (<ListItemText>{this.props.item.title}</ListItemText>);
+              case "Planet":
+                return (<ListItemText>{this.props.item.name}</ListItemText>);
+              case "Species":
+                return (<ListItemText>{this.props.item.name}</ListItemText>);
+              case "Starship":
+                return (<ListItemText>{this.props.item.name}</ListItemText>);
+              case "Vehicle":
+                return (<ListItemText>{this.props.item.name}</ListItemText>);
+              default:
+                return null;
+            }
+          })()}
+        </ListItem>
+        <Modal
+          aria-labelledby="simple-modal-title"
+          aria-describedby="simple-modal-description"
+          open={this.state.open}
+          onClose={() => this.closeItemDetails()}
+          style={this.modalStyle}
+        >
+          <div style={this.modalContentStyle}>
+            {
+              Object.entries(this.props.item).map((entry, index) => {
+                return <div key={index}>
+                  {this.toCapitalizedWords(entry[0])}: {entry[1]}
+                </div>
+              })
+            }
+          </div>
+        </Modal>
+      </span>
     );
   }
 }
